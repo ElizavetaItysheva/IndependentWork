ce948f9f3de0d757dda0e7da7ba4d7297549831a
June <june122@inbox.ru>
2022-12-02 13:47:00 +0300

    Ввела переменную, которая обозначает проверяемое число

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Самостоятельная работа~");
         double number1 = 5.573;
-        double number2 = 5.556;
-        boolean fisrtNumber = (number1 % 10.0) > (number2 % 10.0);
+        double number2 = 5.622;
+        int number = 10;
+        boolean fisrtNumber = (number1 % number) > (number2 % number);
         if (fisrtNumber) {
             System.out.println("Число " + number1 + " ближе к 10.");
         } else {
